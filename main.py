05453596a00cdcecab33179024190450732ba778
Jay Bretherton <jake.bretherton05@gmail.com>
2023-03-06 18:03:06 +0000

    RK4

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 from Vector import Vector
 
 
-def calclate():
+def calculate():
     time_step = 0.08
     number_of_iterations = 800
 
-    angular_momenta = [Vector() for i in range(number_of_iterations)]
+    angular_momenta = [Vector() for _ in range(number_of_iterations)]
 
     time_measurements = [float(0)] * number_of_iterations
 
@@ -29,17 +28,20 @@ def calclate():
     # todo rename this
     principle_moment = Vector()
 
-    principle_moment.x = (inertial_tensor_principal_axis.z - inertial_tensor_principal_axis.y) / inertial_tensor_principal_axis.x
-    principle_moment.y = (inertial_tensor_principal_axis.x - inertial_tensor_principal_axis.z) / inertial_tensor_principal_axis.y
-    principle_moment.z = (inertial_tensor_principal_axis.y - inertial_tensor_principal_axis.x) / inertial_tensor_principal_axis.z
+    principle_moment.x = (
+                                 inertial_tensor_principal_axis.z - inertial_tensor_principal_axis.y) / inertial_tensor_principal_axis.x
+    principle_moment.y = (
+                                 inertial_tensor_principal_axis.x - inertial_tensor_principal_axis.z) / inertial_tensor_principal_axis.y
+    principle_moment.z = (
+                                 inertial_tensor_principal_axis.y - inertial_tensor_principal_axis.x) / inertial_tensor_principal_axis.z
 
     for n in range(number_of_iterations - 1):
         current_angular_momentum = angular_momenta[n]
 
-        runge_kutta2 = perform_runge_kutta(current_angular_momentum, principle_moment, time_step)
+        runge_kutta_total = perform_runge_kutta(current_angular_momentum, principle_moment, time_step)
 
         next_angular_momentum = angular_momenta[n + 1]
-        next_angular_momentum.elements = current_angular_momentum.elements + runge_kutta2.elements
+        next_angular_momentum.elements = current_angular_momentum.elements + runge_kutta_total.elements
 
         time_measurements[n + 1] = n * time_step
 
@@ -49,34 +51,27 @@ def calclate():
 def perform_runge_kutta(current_angular_momentum, principle_moment, time_step):
     runge_kutta1 = runge_kutta_one(time_step, principle_moment, current_angular_momentum)
     runge_kutta2 = runge_kutta_two(current_angular_momentum, principle_moment, runge_kutta1, time_step)
-
     runge_kutta3 = runge_kutta_three(current_angular_momentum, principle_moment, runge_kutta2, time_step)
+    runge_kutta4 = runge_kutta_four(current_angular_momentum, principle_moment, runge_kutta3, time_step)
 
-    return runge_kutta3
+    runge_kutta_total = Vector()
+
+    runge_kutta_total.elements = ((1.0 / 6.0)
+                                  * (runge_kutta1.elements
+                                     + (2 * runge_kutta2.elements)
+                                     + (2 * runge_kutta3.elements)
+                                     + runge_kutta4.elements)
+                                  )
+
+    return runge_kutta_total
 
 
-def runge_kutta_three(current_angular_momentum, principle_moment, runge_kutta2, time_step):
-    runge_kutta3 = Vector()
-    runge_kutta3.x = (
-            -time_step
-            * principle_moment.x
-            * (current_angular_momentum.y + 0.5 * runge_kutta2.y)
-            * (current_angular_momentum.z + 0.5 * runge_kutta2.z)
-    )
-    runge_kutta3.y = (
-            -time_step
-            * principle_moment.y
-            * (current_angular_momentum.x + 0.5 * runge_kutta2.x)
-            * (current_angular_momentum.z + 0.5 * runge_kutta2.z)
-    )
-    runge_kutta3.z = (
-            -time_step
-            * principle_moment.z
-            * (current_angular_momentum.x + 0.5 * runge_kutta2.x)
-            * (current_angular_momentum.y + 0.5 * runge_kutta2.y)
-    )
-
-    return runge_kutta3
+def runge_kutta_one(time_step, principle_moment, current_angular_momentum) -> Vector:
+    runge_kutta1 = Vector()
+    runge_kutta1.x = - time_step * principle_moment.x * current_angular_momentum.y * current_angular_momentum.z
+    runge_kutta1.y = - time_step * principle_moment.y * current_angular_momentum.x * current_angular_momentum.z
+    runge_kutta1.z = - time_step * principle_moment.z * current_angular_momentum.x * current_angular_momentum.y
+    return runge_kutta1
 
 
 def runge_kutta_two(current_angular_momentum, principle_moment, runge_kutta1, time_step) -> Vector:
@@ -105,12 +100,52 @@ def runge_kutta_two(current_angular_momentum, principle_moment, runge_kutta1, ti
     return runge_kutta2
 
 
-def runge_kutta_one(time_step, principle_moment, current_angular_momentum) -> Vector:
-    runge_kutta1 = Vector()
-    runge_kutta1.x = - time_step * principle_moment.x * current_angular_momentum.y * current_angular_momentum.z
-    runge_kutta1.y = - time_step * principle_moment.y * current_angular_momentum.x * current_angular_momentum.z
-    runge_kutta1.z = - time_step * principle_moment.z * current_angular_momentum.x * current_angular_momentum.y
-    return runge_kutta1
+def runge_kutta_three(current_angular_momentum, principle_moment, runge_kutta2, time_step):
+    runge_kutta3 = Vector()
+    runge_kutta3.x = (
+            -time_step
+            * principle_moment.x
+            * (current_angular_momentum.y + 0.5 * runge_kutta2.y)
+            * (current_angular_momentum.z + 0.5 * runge_kutta2.z)
+    )
+    runge_kutta3.y = (
+            -time_step
+            * principle_moment.y
+            * (current_angular_momentum.x + 0.5 * runge_kutta2.x)
+            * (current_angular_momentum.z + 0.5 * runge_kutta2.z)
+    )
+    runge_kutta3.z = (
+            -time_step
+            * principle_moment.z
+            * (current_angular_momentum.x + 0.5 * runge_kutta2.x)
+            * (current_angular_momentum.y + 0.5 * runge_kutta2.y)
+    )
+
+    return runge_kutta3
+
+
+def runge_kutta_four(current_angular_momentum, principle_moment, runge_kutta3, time_step):
+    runge_kutta4 = Vector()
+
+    runge_kutta4.x = (
+            -time_step
+            * principle_moment.x
+            * (current_angular_momentum.y + runge_kutta3.y)
+            * (current_angular_momentum.z + runge_kutta3.z)
+    )
+    runge_kutta4.y = (
+            -time_step
+            * principle_moment.y
+            * (current_angular_momentum.x + runge_kutta3.x)
+            * (current_angular_momentum.z + runge_kutta3.z)
+    )
+    runge_kutta4.z = (
+            -time_step
+            * principle_moment.z
+            * (current_angular_momentum.x + runge_kutta3.x)
+            * (current_angular_momentum.y + runge_kutta3.y)
+    )
+    return runge_kutta4
 
 
 def plot(time_measurements, angular_momenta):
@@ -120,7 +155,7 @@ def plot(time_measurements, angular_momenta):
     plt.show()
 
 
-plot(*calclate())
+plot(*calculate())
 
 # To summarise the algorithm:
 # • Lines 2 to 11: define known constants, the stepsize, the total number of iterative
