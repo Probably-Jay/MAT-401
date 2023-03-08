4a39d15ef0f3a9711d801b8737552750bdd30d79
Jay Bretherton <jake.bretherton05@gmail.com>
2023-03-08 01:52:01 +0000

    comments

--- task 3.py
+++ task 3.py
@@ -27,18 +27,32 @@ def calculate():
 
     # run simulation
     for n in range(number_of_iterations - 1):
+        # perform euler's method to calculate integration of acceleration into velocity
         current_velocity = velocity[n]
         next_velocity = velocity[n + 1]
-        next_velocity.elements = current_velocity.elements + (acceleration.elements * time_step)
+        next_velocity.elements = euler_method(current_velocity, acceleration, time_step)
 
+        # perform euler's method to calculate integration of velocity into position
+        # uses current position and next velocity, therefore is semi-implicit
         current_position = position[n]
         next_position = position[n + 1]
-        next_position.elements = current_position.elements + (next_velocity.elements * time_step)
+        next_position.elements = euler_method(current_position, next_velocity, time_step)
 
         time_measurements[n + 1] = n * time_step
 
     # return results
     return time_measurements, position, velocity
+
+
+def euler_method(current_element, derivative_element, time_step):
+    """
+    Performs euler's method calculation
+    :param derivative_element: the element controlling the rate of change of the function
+    :param current_element: the current value of the function
+    :param time_step: the delta time of the calculation
+    :return: the next value of the function; at the time of 'current_element' + time_step
+    """
+    return current_element.elements + (derivative_element.elements * time_step)
 
 
 def plot(time_measurements, position, velocity):
